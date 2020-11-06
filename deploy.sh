e3a666c0c23e97bb376accbb8f7b32a084d0e347
J Munoz <juan.munoz@log3smart.com>
2020-11-05 19:19:54 -0500

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t jldocker954/multi-client:latest -t jldocker954/multi-client:$SHA -f ./client/Dockerfile ./client 
+docker build -t jldocker954/multi-server:latest -t jldocker954/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jldocker954/multi-worker:latest -t jldocker954/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jldocker954/multi-client:latest
+docker push jldocker954/multi-server:latest
+docker push jldocker954/multi-worker:latest
+
+docker push jldocker954/multi-client:$SHA
+docker push jldocker954/multi-server:$SHA
+docker push jldocker954/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=jldocker954/multi-server:$SHA
+kunectl set image deployments/client-deployment client=jldocker954/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jldocker954/multi-worker:$SHA
